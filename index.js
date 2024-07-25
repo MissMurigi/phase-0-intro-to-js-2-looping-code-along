183b9f877ffc8911be1de8028443822a6a947823
MissMurigi <vickymurigi001@gmail.com>
2024-07-25 23:23:56 +0300

    LCA

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names = ["John ", "Doe", "Mwaura"];
+
+function writeCards(arrNames, eventName) {
+  const messages = [];
+  for (let i = 0; i < arrNames.length; i++) {
+    messages.push(
+      `Thank you, ${arrNames[i]}, for the wonderful ${eventName} gift!`
+    );
+  }
+  return messages;
+}
+
+function countDown(start) {
+  let count = start;
+  while(count >= 0) {
+    console.log(count);
+    count--;
+  }
+}
